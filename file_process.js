6c47d24c9a8d9afad9f0e7953b8269c2e1dbce74
KW <weinerk@gmail.com>
2013-01-30 14:51:58 +0200

    add input processing + CTRL-Q key shortcut for timestamp

--- file_process.js
+++ file_process.js
@@ -35,3 +35,38 @@ window.onload = function() {
     //};
 
 };
+
+
+function processKeyDown(event)
+{
+		if(event.ctrlKey && event.keyCode==81) //CTRL-Q
+		{
+				var date = new Date;
+
+				var YY = date.getFullYear();
+				var MM = date.getMonth()+1; // beware: January = 0; February = 1, etc.
+				var DD = date.getDate();
+				var hh = date.getHours();
+				var mm = date.getMinutes();
+				var ss = date.getSeconds();
+
+				MM = ("0"+MM).slice(-2);
+				DD = ("0"+DD).slice(-2);
+				hh = ("0"+hh).slice(-2);
+				mm = ("0"+mm).slice(-2);
+				ss = ("0"+ss).slice(-2);
+
+				var textToInsert = ""+YY+MM+DD+"_"+hh+mm+ss+"\n";
+
+				var eventObject = document.createEvent('TextEvent');
+				eventObject.initTextEvent('textInput',
+																	true,
+																	true,
+																	null,
+																	textToInsert);
+
+				var pad = document.getElementById('pad');
+				pad.dispatchEvent(eventObject);
+		}
+}
+

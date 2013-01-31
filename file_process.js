cf222dfd4c0171be0cba317ea8a190e83fa2b3e3
KW <weinerk@gmail.com>
2013-01-31 11:35:12 +0200

    changes: move saveProgress() and processKeyDown() back into onload handler - and add them with addEventListener otherwise cant load it as chrome extention - get this error Refused to execute inline script because it violates the following Content Security Policy directive: "script-src 'self' chrome-extension-resource:" not allowed to execute inline scripts - only sourced from an external file.
    simplified delayed save
    
    some cleanup

--- file_process.js
+++ file_process.js
@@ -1,17 +1,3 @@
-function saveProgress() {
-    var pad = document.getElementById('pad');
-    localStorage.textpad_buffer = pad.innerHTML;
-};
-
-var delay = (function(){
-  var timer = 0;
-  return function(callback, ms){
-    clearTimeout (timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
-
-
 window.onload = function() {
     
     var pad = document.getElementById('pad');
@@ -23,67 +9,84 @@ window.onload = function() {
         localStorage.textpad_buffer = pad.innerHTML;
     }
 
-    function handleWindowBlur() {
-        localStorage.textpad_buffer = pad.innerHTML;
-    };
-    //window.addEventListener("focus", handleWindowActive);
-
+    //function handleWindowBlur() {
+    //    localStorage.textpad_buffer = pad.innerHTML;
+    //};
     //window.addEventListener("blur", handleWindowBlur);
 
     //window.onbeforeunload = function() {
     //    saveProgress();
     //};
 
-};
+		function saveProgress() {
+				var pad = document.getElementById('pad');
+				localStorage.textpad_buffer = pad.innerHTML;
+        //alert("saved");
+		};
 
+		pad.addEventListener("keyup", 
+                         function(){
+														var timer = 0;
+														clearTimeout (timer);
+														timer = setTimeout(saveProgress, 1000);
+                         }
+												);
 
-function processKeyDown(event)
-{
-		if(event.ctrlKey && event.keyCode==81) //CTRL-Q
+		function processKeyDown(event)
 		{
-				var date = new Date;
+				if(event.ctrlKey && event.keyCode==81) //CTRL-Q
+				{
+						var date = new Date;
 
-				var YY = date.getFullYear();
-				var MM = date.getMonth()+1; // beware: January = 0; February = 1, etc.
-				var DD = date.getDate();
-				var hh = date.getHours();
-				var mm = date.getMinutes();
-				var ss = date.getSeconds();
+						var YY = date.getFullYear();
+						var MM = date.getMonth()+1; // beware: January = 0; February = 1, etc.
+						var DD = date.getDate();
+						var hh = date.getHours();
+						var mm = date.getMinutes();
+						var ss = date.getSeconds();
 
-				MM = ("0"+MM).slice(-2);
-				DD = ("0"+DD).slice(-2);
-				hh = ("0"+hh).slice(-2);
-				mm = ("0"+mm).slice(-2);
-				ss = ("0"+ss).slice(-2);
+						MM = ("0"+MM).slice(-2);
+						DD = ("0"+DD).slice(-2);
+						hh = ("0"+hh).slice(-2);
+						mm = ("0"+mm).slice(-2);
+						ss = ("0"+ss).slice(-2);
 
-				var textToInsert = ""+YY+MM+DD+"_"+hh+mm+ss+"\n";
+						var textToInsert = ""+YY+MM+DD+"_"+hh+mm+ss+"\n";
 
-				var eventObject = document.createEvent('TextEvent');
-				eventObject.initTextEvent('textInput',
-																	true,
-																	true,
-																	null,
-																	textToInsert);
+						var eventObject = document.createEvent('TextEvent');
+						eventObject.initTextEvent('textInput',
+																			true,
+																			true,
+																			null,
+																			textToInsert);
 
-				var pad = document.getElementById('pad');
-				pad.dispatchEvent(eventObject);
+						var pad = document.getElementById('pad');
+						pad.dispatchEvent(eventObject);
+				}
+				
+				if(event.keyCode==9) //TAB
+				{ 
+						var textToInsert = "\t";
+
+						var eventObject = document.createEvent('TextEvent');
+						eventObject.initTextEvent('textInput',
+																			true,
+																			true,
+																			null,
+																			textToInsert);
+
+						var pad = document.getElementById('pad');
+						pad.dispatchEvent(eventObject);
+
+						event.preventDefault();
+				}
 		}
-		
-		if(event.keyCode==9) //TAB
-		{ 
-				var textToInsert = "\t";
 
-				var eventObject = document.createEvent('TextEvent');
-				eventObject.initTextEvent('textInput',
-																	true,
-																	true,
-																	null,
-																	textToInsert);
+		pad.addEventListener("keydown", 
+                         function(){
+														 processKeyDown(event);
+                         }
+                        );
 
-				var pad = document.getElementById('pad');
-				pad.dispatchEvent(eventObject);
 
-				event.preventDefault();
-		}
-}
-
+};
